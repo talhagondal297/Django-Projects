25b5536acd359139d8a077ff682b3236194bcd84
talhagondal297 <talha2gondal1000@gmail.com>
2024-05-24 12:54:02 +0500

    store and carts functionality

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -23,7 +23,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name = 'home'),
-    path('store/', include('store.urls'))
+    path('store/', include('store.urls')),
+    path('cart/',include('carts.urls')),
     
     
 ] 
